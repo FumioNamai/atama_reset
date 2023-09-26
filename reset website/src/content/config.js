d8183a35da60d5397748192bfb29997e59988688
FumioNamai <baggio10.namai@gmail.com>
2023-09-27 08:36:09 +0900

    paginateコンポーネントの横幅を調整

---
+++ reset website/src/content/config.js
@@ -0,0 +1,14 @@
+import { defineCollection } from 'astro:content'
+
+const blogCollection = defineCollection({
+  schema: {
+    collections: [
+      {
+        name:
+      }
+    ]
+  }
+})
+export const collection = {
+  'blog':blogCollection,
+}
