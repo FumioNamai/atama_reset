2c389745e64e5600e0f2bd23daa2750449deb4c3
FumioNamai <baggio10.namai@gmail.com>
2023-12-05 00:08:26 +0900

    NewsListのline-hightを修正

--- reset website/src/library/constants.js
+++ reset website/src/library/constants.js
@@ -1,9 +1,8 @@
 export const siteMeta = {
   siteTitle: '頭ほぐし専門店reset',
   siteDesc: '',
-  // siteUrl: 'https://atama-reset.com',
-  siteUrl: 'http://localhost:4321/',
-  // siteUrl: 'https://main--magenta-jalebi-d1fcd1.netlify.app/',
+  siteUrl: 'https://atama-reset.com',
+  // siteUrl: 'http://localhost:4321/',
   siteType: 'website',
   siteLocale: 'ja_JP',
   siteIcon: '',
