6df14de0062dffe9d28340648476be16a8df28d1
FumioNamai <baggio10.namai@gmail.com>
2023-12-06 23:28:15 +0900

    astrojs/sitemapをインストール

--- reset website/astro.config.mjs
+++ reset website/astro.config.mjs
@@ -1,21 +1,24 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify/functions";
-
 import preact from "@astrojs/preact";
-
 import { siteMeta } from './src/library/constants';
-const { siteUrl } = siteMeta;
+import sitemap from "@astrojs/sitemap";
+const {
+  siteUrl
+} = siteMeta;
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact({
     compat: true
-  })],
+  }), sitemap()],
   image: {
-    remotePatterns:[{ protocol: "https" }]
+    remotePatterns: [{
+      protocol: "https"
+    }]
   },
   output: "server",
   adapter: netlify(),
-  site: siteUrl,
+  site: siteUrl
 });
